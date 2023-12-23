e74e482057c34121ae48f5d7bc2dd7620e16b08b
Le Roy Julie <julieleroy317@gmail.com>
2023-12-23 13:25:27 +0100

    ptit changement

--- src/gui/Game.java
+++ src/gui/Game.java
@@ -10,9 +10,21 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 import java.awt.Color;
+import java.awt.Dimension;
 
 public class Game extends JPanel implements Frames{
     
+     //Tiles
+    final int scale =2;
+    final int initialTileSize = 16;
+    final int tileSize= initialTileSize*scale; //32x32
+    
+    //Ecran
+    final int col = 30;
+    final int ligne= 20;
+    final int width= col*tileSize; //960
+    final int height= ligne*tileSize; //640
+
     private Random random = new Random();
     private BufferedImage image;
 
@@ -21,24 +33,23 @@ public class Game extends JPanel implements Frames{
     public Game(BufferedImage img){
         this.image=img;
         addAsset();
+        setPreferredSize(new Dimension(width, height));
         setVisible(true);
 
     }
 
     public void paintComponent(Graphics g)  {
         super.paintComponent(g);
-        for(int x=0;x<21;x++){
-            for(int y=0;y<20;y++){
-                g.drawImage(assets.get(random.nextInt(5)),x*30, y*30, 64, 64, null);
-                //g.setColor(getColor());
-                //g.fillRect(x*30, y*30, 64, 64);
+        for(int x=0;x<col;x++){
+            for(int y=0;y<ligne;y++){
+                g.drawImage(assets.get(random.nextInt(5)),x*tileSize, y*tileSize, tileSize, tileSize, null);
             }
         }
     }
 
     public void addAsset(){
         for(int x=0;x<5;x++){
-            assets.add(image.getSubimage(x*16, 0, 16, 16));
+            assets.add(image.getSubimage(x*initialTileSize, 0, initialTileSize, initialTileSize));
         }
     }
 
