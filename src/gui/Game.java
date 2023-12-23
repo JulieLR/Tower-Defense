fbb12b8d152ca69410135c77a98e9ffacfb3fdea
Le Roy Julie <julieleroy317@gmail.com>
2023-12-23 21:01:06 +0100

    map

--- src/gui/Game.java
+++ src/gui/Game.java
@@ -3,6 +3,8 @@ package gui;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
+import config.MapConfig;
+
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -12,56 +14,59 @@ import java.util.Random;
 import java.awt.Color;
 import java.awt.Dimension;
 
-public class Game extends JPanel implements Frames{
+public class Game extends JPanel{
     
      //Tiles
-    final int scale =2;
+    final int scale =4;
     final int initialTileSize = 16;
+
     final int tileSize= initialTileSize*scale; //32x32
-    
+
     //Ecran
-    final int col = 30;
-    final int ligne= 20;
+    final int col = 15;
+    final int ligne= 10;
+
     final int width= col*tileSize; //960
     final int height= ligne*tileSize; //640
 
-    private Random random = new Random();
     private BufferedImage image;
-
-    private ArrayList<BufferedImage> assets = new ArrayList<>();
+    private MapConfig tiles;
 
     public Game(BufferedImage img){
         this.image=img;
-        addAsset();
+        this.tiles= new MapConfig(this);
+
         setPreferredSize(new Dimension(width, height));
         setVisible(true);
 
     }
 
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public int getInitialTileSize() {
+        return initialTileSize;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public int getLigne() {
+        return ligne;
+    }
+
+    public int getTileSize() {
+        return tileSize;
+    }
+
     public void paintComponent(Graphics g)  {
         super.paintComponent(g);
-        for(int x=0;x<col;x++){
-            for(int y=0;y<ligne;y++){
-                g.drawImage(assets.get(random.nextInt(5)),x*tileSize, y*tileSize, tileSize, tileSize, null);
-            }
-        }
-    }
 
-    public void addAsset(){
-        for(int x=0;x<5;x++){
-            assets.add(image.getSubimage(x*initialTileSize, 0, initialTileSize, initialTileSize));
-        }
-    }
+        tiles.drawBackground(g); //draw map à mettre avant le draw des characters
 
-    public int getRandomNb(){
-        return random.nextInt(3);
-    }
-
-    public Color getColor(){
-        int r = random.nextInt(256);
-        int g = random.nextInt(256);
-        int b = random.nextInt(256);
-        return new Color(r,g,b);
+        g.dispose(); //
     }
 
 }
