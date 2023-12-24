4c933e8244a0abd1a1ca7c1159884d1bba4724c2
WENG Natacha <natachaweng24@gmail.com>
2023-12-24 19:57:51 +0100

    implementation des interactions avec le clavier et la souris (début)

--- src/gui/App.java
+++ src/gui/App.java
@@ -2,9 +2,14 @@ package gui;
 
 import javax.swing.JFrame;
 
+import inputs.Keyboard_Listener;
+import inputs.Mouse_Listener;
+
 public class App extends JFrame{
 
     private Game game;
+    private Mouse_Listener mouseListener; 
+    private Keyboard_Listener keyboardlistener;
 
     public App(){
         
@@ -20,9 +25,21 @@ public class App extends JFrame{
         setVisible(true);
     }
 
+    public void initInput() {
+        mouseListener= new Mouse_Listener();
+        keyboardlistener= new Keyboard_Listener();
+
+        addMouseListener(mouseListener);
+        addMouseMotionListener(mouseListener);
+        addKeyListener(keyboardlistener);
+
+        requestFocus();
+    }
+
     public static void main(String[] args) {
         System.out.println("Hey");
         App app = new App();
+        app.initInput();
         //Menu menu = new Menu();
     }
 }
