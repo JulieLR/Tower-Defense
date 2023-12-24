61d373aac5ea3e032a3096f5e45096c81e04dfc2
WENG Natacha <natachaweng24@gmail.com>
2023-12-25 00:56:53 +0100

    config tower

---
+++ src/gui/TowerConfig.java
@@ -0,0 +1,21 @@
+package gui;
+
+import java.awt.image.BufferedImage;
+
+import config.MapConfig;
+
+public class TowerConfig {
+    private BufferedImage[] towerImage;
+
+    public TowerConfig () {
+        loadTowerImages();
+    }
+
+    private void loadTowerImages () {
+        towerImage= new BufferedImage[6];
+        
+        
+    }
+
+
+}
