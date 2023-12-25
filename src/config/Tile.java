58623e24160648bb714e6d6201b0e7e76f9eeb19
Le Roy Julie <julieleroy317@gmail.com>
2023-12-25 19:53:40 +0100

    affichage un enemy + son deplacement

--- src/config/Tile.java
+++ src/config/Tile.java
@@ -2,6 +2,8 @@ package config;
 
 import java.awt.image.BufferedImage;
 
+import model.Coordinates;
+
 public class Tile {
 
     public enum Type{PATH,GRASS,WATER,CASTLE,TOWER,BAR};
@@ -10,13 +12,19 @@ public class Tile {
 
     private boolean isPath=false;
     private int value;
+    private Coordinates tileCoor;
+
+    public Coordinates getTileCoor() {
+        return tileCoor;
+    }
 
     private Type type;
 
-    public Tile(BufferedImage img, Type type, int v){
+    public Tile(BufferedImage img, Type type, int v,Coordinates coor){
         this.image=img;
         this.type=type;
         this.value=v;
+        this.tileCoor=coor;
     }
 
     public Tile(BufferedImage img,BufferedImage sImg, Type type, int v){
@@ -40,5 +48,30 @@ public class Tile {
     
     public BufferedImage getSecondImg() {
         return secondImg;
+    }
+
+    public boolean isPath(){
+        return(this.type == Type.PATH);
+    }
+
+    public String typeToString(){
+        String s="";
+        switch(this.type){
+            case BAR: s="BAR";
+                break;
+            case CASTLE: s="CASTLE";
+                break;
+            case GRASS: s="GRASS";
+                break;
+            case PATH: s="PATH";
+                break;
+            case TOWER: s="TOWER";
+                break;
+            case WATER: s="WATER";
+                break;
+            default:
+                break;
+        }
+    return s;
     }
 }
