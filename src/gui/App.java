c4e93c363b1510a7197538c24b21360d525db5f6
WENG Natacha <natachaweng24@gmail.com>
2023-12-25 19:28:01 +0100

    serialize+ deserialize

--- src/gui/App.java
+++ src/gui/App.java
@@ -20,7 +20,7 @@ public class App extends JFrame{
         setVisible(true);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hey");
         App app = new App();
         app.game.initInput();
