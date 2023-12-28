0b99015b1adb270e65e02f7b419196fd26027f56
WENG Natacha <natachaweng24@gmail.com>
2023-12-28 20:17:22 +0100

    affichage (commencement)

--- src/config/MapConfig.java
+++ src/config/MapConfig.java
@@ -16,6 +16,7 @@ public class MapConfig {
     private Tile end;
     private Coordinates endCoor;
     private Coordinates start;
+    private Coordinates[] posTower;
 
 
     Random random = new Random();
@@ -107,6 +108,8 @@ public class MapConfig {
                     }
                     else if(readline.charAt(i) == 'T'){//Tower place
                         this.map[ligne][col] = new Tile(17,18, Type.TOWER,Character.getNumericValue(readline2.charAt(i)),new Coordinates(ligne, col));
+                        this.posTower[nbTower]= new Coordinates(ligne, col);
+                        nbTower++;
                         ligne++;
                     }
                     else if(readline.charAt(i) == 'S'){//ennemies start
