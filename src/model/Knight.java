c1f4e2acf0a3a6af057f990f69c3787ee7d62a45
Le Roy Julie <julieleroy317@gmail.com>
2023-12-28 20:40:27 +0100

    Knight

--- src/model/Knight.java
+++ src/model/Knight.java
@@ -2,8 +2,10 @@ package model;
 
 public class Knight extends Enemy {
 
-    public Knight(int vie, Coordinates pos, int degat, float vitesseAtk, int type) {
-        super(vie, pos, degat, vitesseAtk, type);
+    private int rang;
+
+    public Knight(int n, Coordinates pos) {
+        super(200, pos, 0, 0.5f, 0);
         //TODO Auto-generated constructor stub
     }
     
