d7d67928cb22de4c5cb764224edf5868f6a80bd5
WENG Natacha <natachaweng24@gmail.com>
2023-12-28 23:02:39 +0100

    affichage et positionnement des tours aux bons endroits

--- src/config/MapConfig.java
+++ src/config/MapConfig.java
@@ -16,7 +16,6 @@ public class MapConfig {
     private Tile end;
     private Coordinates endCoor;
     private Coordinates start;
-    //private Coordinates[] posTower;
 
 
     Random random = new Random();
@@ -54,11 +53,6 @@ public class MapConfig {
     public Coordinates getStart() {
         return start;
     }
-
-    /* public Coordinates[] getPosTower () {
-        return this.posTower;
-    } */
-
     
     public void mapFromFile(String path, String path2){
         try{
@@ -113,8 +107,6 @@ public class MapConfig {
                     }
                     else if(readline.charAt(i) == 'T'){//Tower place
                         this.map[ligne][col] = new Tile(17,18, Type.TOWER,Character.getNumericValue(readline2.charAt(i)),new Coordinates(ligne, col));
-                        //this.posTower[nbTower]= new Coordinates(ligne, col);
-                        //nbTower++;
                         ligne++;
                     }
                     else if(readline.charAt(i) == 'S'){//ennemies start
