725e6894a7f9d1b5573c0d04d5c715cbf6a4458d
WENG Natacha <natachaweng24@gmail.com>
2023-12-28 17:22:49 +0100

    debut config Tower

--- src/model/Tower.java
+++ src/model/Tower.java
@@ -61,18 +61,6 @@ public Tower(/* int vie, */ int prix, Coordinates pos,int degat, int vitesseAtk,
     /* public boolean isDead () {
         return this.pointDeVie<=0;
     } */
-    
-    // attaque
-    public void attaque (Entities p) {
-        //p.pointDeVie=-super.getDegat();
-    }
-
-    // distance entre les deux personnes 
-   /*  public double disBetween (Personnages p) {
-        return Math.sqrt(
-        Math.pow(p.position.x()-this.position.x(),2) +
-        Math.pow(p.position.y()-this.position.y(),2));
-    }*/
 
 
 }
