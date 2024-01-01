82d0d50f097e1ea06beb2f7db724f801b44f0d5d
WENG Natacha <natachaweng24@gmail.com>
2024-01-01 19:12:23 +0100

    changement de type des paramètres pour les angles

--- src/gui/TowerGraphics.java
+++ src/gui/TowerGraphics.java
@@ -41,9 +41,10 @@ public class TowerGraphics implements Graphic,Frame {
         double[] tab= new double[2]; 
         tab[0]= Math.cos(-corner); // sens horaire, donc on doit mettre le signe - avant la valeur de l'angle
         tab[1]= Math.sin(-corner); 
-        return tab;
-         
+        return tab;    
     }
+
+    
 
     public Direction getVerticalPos(Coordinates t, Coordinates e){
         if(e.getY()<t.getY()){
@@ -115,8 +116,8 @@ public class TowerGraphics implements Graphic,Frame {
         }
     }
 
-    public double angle(Tower t, Enemy e){
-        double angle = Math.abs(Math.abs(t.getPos().getX())-Math.abs(e.getPos().getX())/Math.abs(t.getPos().getY())-Math.abs(e.getPos().getY()));
+    public double angle(Coordinates t, Coordinates e){
+        double angle = Math.abs(Math.abs(t.getX())-Math.abs(e.getX())/Math.abs(t.getY())-Math.abs(e.getY()));
         return angle;
     }
 
@@ -133,7 +134,7 @@ public class TowerGraphics implements Graphic,Frame {
         Coordinates e = this.game.getEnemyConfig().getNextCoor((t.getTarget()));
         float y = getVerticalNombre(t.getPos(), t.getTarget().getPos());
         float x = getHorizontalNombre(t.getPos(), t.getTarget().getPos());
-        double angle =getRotateCoef(t.getPos(), t.getTarget().getPos())*90+Math.toDegrees(Math.atan(this.angle(t, t.getTarget())));
+        double angle =getRotateCoef(t.getPos(), t.getTarget().getPos())*90+Math.toDegrees(Math.atan(this.angle(t.getPos(), t.getTarget().getPos())));
 
         
         int nb=6;
@@ -183,7 +184,7 @@ public class TowerGraphics implements Graphic,Frame {
              }
 
             }
-            System.out.println(Math.toDegrees(Math.atan(this.angle(t, t.getTarget()))));
+            System.out.println(Math.toDegrees(Math.atan(this.angle(t.getPos(), t.getTarget().getPos()))));
         }}
     }
 
