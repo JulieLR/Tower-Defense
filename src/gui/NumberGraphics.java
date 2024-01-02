a428c105d7ac5c6a0835289780b3f1153c4b410c
WENG Natacha <natachaweng24@gmail.com>
2024-01-02 18:56:25 +0100

    affichage sans les 0 au début et 0 quand la base a plus de vie

--- src/gui/NumberGraphics.java
+++ src/gui/NumberGraphics.java
@@ -21,7 +21,7 @@ public class NumberGraphics implements Graphic{
 
     @Override
     public void addAsset() {
-        for (int ligne=0; ligne<10; ligne++) {
+        for (int ligne=0; ligne<11; ligne++) { // image transparente
             numberAsset.add(this.game.getNumberImage().getSubimage(ligne*9, 0, 9, 12));
         }
         numberAsset.add(this.game.getAllSpriteImage().getSubimage(16*9, 16*7, 16, 16));
@@ -29,13 +29,25 @@ public class NumberGraphics implements Graphic{
 
     @Override
     public void drawImages(Graphics g) {
-        int life_tmp=this.base.getPointDeVie();
-        for (int i=3; i>=0; i--) {
-            int chiffre=life_tmp%10;
-            g.drawImage(this.numberAsset.get(chiffre), 20+i*40, 15, 45, 60, null);
-            life_tmp/=10;
-        } 
-        g.drawImage(this.numberAsset.get(10), 190, 20, 45, 45, null);
+        if (this.game.getBase().getPointDeVie()==0) {
+           g.drawImage(this.numberAsset.get(0), 80*32, 15, 36, 48, null);
+        }
+        else {
+            int life_tmp= this.base.getPointDeVie();
+            int j= 3; 
+            int div=1000;
+            while ((life_tmp/div)%10==0 && j>0) {
+                j--;
+                div/=10;
+
+            }
+            for (int i=j; i>=0; i--) {
+                int chiffre=life_tmp%10;
+                g.drawImage(this.numberAsset.get(chiffre), 80+i*32, 15, 36, 48, null);
+                life_tmp/=10;
+            } 
+        }
+        g.drawImage(this.numberAsset.get(11), 20, 10, 54, 54, null);
 
     }
 
