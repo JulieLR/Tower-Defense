4c378381214e7f9888ad1bb66b212a16653b5d34
Le Roy Julie <julieleroy317@gmail.com>
2024-01-03 14:06:27 +0100

    changement emplacement images et ajout slime

--- src/gui/Graphic.java
+++ src/gui/Graphic.java
@@ -1,8 +1,23 @@
 package gui;
 
+import javax.imageio.ImageIO;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public interface Graphic {
+
     public void addAsset();
     public void drawImages(Graphics g);
+
+    public default BufferedImage getImage(String chemin){
+        try {
+            BufferedImage image = ImageIO.read(new File(chemin));
+            return image;
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
