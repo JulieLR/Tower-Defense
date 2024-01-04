11240c4f7b5d8a8440399f1bad542168f0d56fc3
Le Roy Julie <julieleroy317@gmail.com>
2024-01-04 21:38:48 +0100

    tours attaque

--- src/model/Knight.java
+++ src/model/Knight.java
@@ -7,13 +7,13 @@ public class Knight extends Enemy {
     private int rang;
 
     public Knight(int n, Coordinates pos,Game game) {
-        super(500, pos, 1, 1f, 0,1,game);
+        super(500, pos, 1, 1f, 0,10,game);
         this.rang=n;
         if(rang==1){
             super.setPointDeVie(400);
             super.setDegat(5);
             super.setVitesseAtk(0.7f);
-            super.setPrime(5);
+            super.setPrime(25);
         }
     }
     
--- src/model/Slime.java
+++ src/model/Slime.java
@@ -8,7 +8,7 @@ public class Slime extends Enemy {
     private int Color;
 
     public Slime(Coordinates pos, Game game) {
-        super(500, pos, 5, 1f, 0, 5, game);
+        super(500, pos, 5, 1f, 0, 15, game);
         Random r = new Random();
         this.Color = r.nextInt(7);
     }
