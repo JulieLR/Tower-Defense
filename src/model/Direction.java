8de93497ac1e16e5c2613594b189e6848caa2e0e
Le Roy Julie <julieleroy317@gmail.com>
2024-01-11 22:13:20 +0100

    changement direction

--- src/model/Direction.java
+++ src/model/Direction.java
@@ -4,21 +4,23 @@ public enum Direction {
     NORTH,SOUTH,EAST,WEST;
 
     public Direction getOppositeDir(){
-        return switch (this) {
-            case NORTH -> Direction.SOUTH;
-            case SOUTH -> Direction.NORTH;
-            case EAST -> Direction.WEST;
-            case WEST -> Direction.EAST;
+        switch (this) {
+            case NORTH : return Direction.SOUTH;
+            case SOUTH : return Direction.NORTH;
+            case EAST : return Direction.WEST;
+            case WEST : return Direction.EAST;
         };
+        return null;
     }
 
     public String DirToString(){
-        return switch(this){
-            case EAST -> "EAST";
-            case NORTH -> "NORTH";
-            case SOUTH -> "SOUTH" ;
-            case WEST -> "WEST";
+        switch(this){
+            case EAST : return"EAST";
+            case NORTH : return"NORTH";
+            case SOUTH : return"SOUTH" ;
+            case WEST : return"WEST";
 
         };
+        return null;
     }
 }
